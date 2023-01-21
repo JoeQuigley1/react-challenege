2747db7a6b9be5e2e90fd86cc8a33155026e6354
JoeQuigley1 <joeyjtq@hotmail.com>
2023-01-21 14:19:42 +0000

    Finish challenge 2 and 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,20 @@
 import css from './App.module.css';
+// import Sidebar from './components/Sidebar';
+import Navbar from './components/NavBarSimple';
+
+// function App() {
+//   return (
+//     <div className={css.App}>
+//       <Sidebar/>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div className={css.App}>
-      {/* Add your components here */}
+    <div>
+      <Navbar/> 
     </div>
-  );
-}
-
+  )
+  }
 export default App;

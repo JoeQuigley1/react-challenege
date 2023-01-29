e35ade498f089d72c8e0e43f9dee3a6eedabca32
JoeQuigley1 <joeyjtq@hotmail.com>
2023-01-29 21:03:42 +0000

    Fix issue with setPosts

--- my-app/src/components/ContentAPIHooks.js
+++ my-app/src/components/ContentAPIHooks.js
@@ -1,11 +1,11 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import css from "./css/Content.module.css";
 import PostItemAPI from "./PostItemAPI";
 import Loader from "./Loader";
 import axios from 'axios'
 import API_KEY from "../secrets";
 
-function ContentAPIHooks() {
+export default function ContentAPIHooks() {
     const [isLoaded, setIsLoaded] = useState(false);
     const [posts, setPosts] = useState([]);
     const [savedPosts, setSavedPosts] = useState([]);
@@ -14,7 +14,7 @@ function ContentAPIHooks() {
         fetchImages()
     }, []);
 
-    const fetchImages = async() => {
+    const fetchImages = async () => {
         const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&per_page=100&safesearch=true&editors_choice=true&orientation=horizontal`);
         const fetchedPosts = response.data.hits;
        
@@ -29,7 +29,7 @@ function ContentAPIHooks() {
             return post.user.toLowerCase().includes(name);
         })
         
-        setSavedPosts(filteredPosts)
+        setPosts(filteredPosts)
     }
 
     
@@ -62,4 +62,3 @@ function ContentAPIHooks() {
         )
     
 }
-export default ContentAPIHooks
